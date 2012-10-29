2235090cf6ac67dadb2bdc6be04ef8e580fb85d0
Daniel Valcarce Silva <daniel.valcarce@udc.es>
2012-10-29 19:12:20 +0100

    Query Formulation y actualizar memoria
     - Implementado un módulo para la formulación de consultas
     - Actualizada la memoria con lo hecho el 29/10/2012

---
+++ src/query_formulation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import nltk
+from nltk.corpus import stopwords
+
+def formulate_query(question):
+	question = question.lower()
+	token_list = question.split()
+
+	stopwords_list = stopwords.words('english')
+	final_list = [w for w in token_list if not (w in stopwords_list)] 
+
+	return " ".join(final_list)
+
+question = raw_input("Write your question: ")
+query = formulate_query(question)
+print query
